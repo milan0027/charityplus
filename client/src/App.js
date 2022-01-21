e38d969a29dc3e0417b487fb7d50fbfe91556499
milan <milanmundhra27@gmail.com>
2022-01-21 15:31:16 +0530

    login register

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,22 @@
-import React, { Fragment } from 'react';
-import {BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import './App.css';
-import { Register } from './components/auth/Register';
-import { Login } from './components/auth/Login';
-import { Landing } from './components/layout/Landing';
-import { Navbar } from './components/layout/Navbar';
+import React, { Fragment } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+import { Register } from "./components/auth/Register";
+import { Login } from "./components/auth/Login";
+import { Landing } from "./components/layout/Landing";
+import { Navbar } from "./components/layout/Navbar";
 
-const App = ()=>
-   <Router>
-     <Fragment>
+const App = () => (
+  <Router>
+    <Fragment>
       <Navbar />
-     
       <Routes>
-
-      <Route  path="/" element={<Landing/>}> </Route>
+        <Route path='/' element={<Landing />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/login' element={<Login />} />
       </Routes>
-      <section className='container'>
-        <Routes>
-              <Route path="/register" element={<Register/>}></Route>
-              <Route path="/login" element={<Login/>}></Route>
-        </Routes>
-     
-      </section>
-     
     </Fragment>
-
-   </Router>
-    
+  </Router>
+);
 
 export default App;
