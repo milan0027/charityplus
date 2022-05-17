0f24e1018015a0db9a98c94a74440d7c51a2f8a9
Arti Yadav <arti2812002@gmail.com>
2022-05-17 23:23:12 +0530

    ultimate final

--- server.js
+++ server.js
@@ -1,8 +1,26 @@
 const express = require('express')
-const connectDB = require('./config/db')
 const app = express()
 const fileUpload = require('express-fileupload');
+const path=require('path')
 //connect database
+const mongoose = require('mongoose')
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+  }
+const db= process.env.DB_URL
+
+const connectDB = async() => {
+    try{
+
+        await mongoose.connect(db,{ useNewUrlParser: true })
+        console.log('MongoDB connected')
+
+    } catch(err){
+        console.log(err.message)
+        //exit process with failure
+        process.exit(1)
+    }
+}
 connectDB()
 
 //init middleware
@@ -20,6 +38,13 @@ app.use('/api/users', require('./routes/api/users'))
 app.use('/api/auth', require('./routes/api/auth'))
 app.use('/api/profile', require('./routes/api/profile'))
 app.use('/api/posts', require('./routes/api/posts'))
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'));
+    app.get('*',(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+    })
+}
 
 const PORT = process.env.PORT || 6000
 
